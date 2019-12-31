16d1b6dd13c5acf6fe32bd593d03f0b428a558fb
Daggerfall-Is-The-Best-TES-Game <Davidc.fried@gmail.com>
2019-12-30 20:17:52 -0500

    code for basic agent and environment

---
+++ main.py
@@ -0,0 +1,41 @@
+from random import random, choice
+
+class Environment:
+    def __init__(self):
+        self.steps_left = 10
+
+    def get_observations(self):
+        return [0.0, 0.0, 0.0]
+
+    def get_actions(self):
+        return [0, 1]
+
+    def is_done(self):
+        return self.steps_left == 0
+
+    def action(self, action):
+        if self.is_done():
+            raise Exception("Game is over")
+        self.steps_left -= 1
+        return random()  #reward for action
+
+
+class Agent:
+    def __init__(self):
+        self.total_reward = 0.0
+
+    def step(self, env):
+        current_obs = env.get_observations()
+        actions = env.get_actions()
+        reward = env.action(choice(actions))
+        self.total_reward += reward
+
+
+if __name__ == "__main__":
+    env = Environment()
+    agent = Agent()
+
+    while not env.is_done():
+        agent.step(env)
+
+    print(f"Total reward got: {agent.total_reward:.4f}")
