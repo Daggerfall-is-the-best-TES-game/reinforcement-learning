bf953d400b5115bc1bd1362ca82255397ce2f979
Daggerfall-Is-The-Best-TES-Game <Davidc.fried@gmail.com>
2019-12-30 23:21:15 -0500

    Random cartpole agent with visuals

---
+++ 02_cartpole_random.py
@@ -0,0 +1,18 @@
+import gym
+
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+    env = gym.wrappers.Monitor(env, "recording", force=True)
+    total_reward = 0.0
+    total_steps = 0
+    obs = env.reset()
+    done = False
+
+    while not done:
+        action = env.action_space.sample()
+        obs, reward, done, _ = env.step(action)
+        total_reward += reward
+        total_steps += 1
+
+    print(f"Episode done in {total_steps:d}, total reward {total_reward:.2f}")
