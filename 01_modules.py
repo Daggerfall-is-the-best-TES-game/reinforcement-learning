0ccb62c96cda7653cc57d2667449753c8bf6bb6e
Daggerfall-Is-The-Best-TES-Game <Davidc.fried@gmail.com>
2020-01-06 14:21:30 -0500

    added libraries to gitignore

---
+++ 01_modules.py
@@ -0,0 +1,26 @@
+from torch import nn, FloatTensor
+
+
+class OurModule(nn.Module):
+    def __init__(self, num_inputs, num_classes, dropout_prob=0.3):
+        super().__init__()
+        self.pipe = nn.Sequential(
+            nn.Linear(num_inputs, 5),
+            nn.ReLU(),
+            nn.Linear(5, 20),
+            nn.ReLU(),
+            nn.Linear(20, num_classes),
+            nn.Dropout(p=dropout_prob),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        return self.pipe(x)
+
+
+if __name__ == "__main__":
+    net = OurModule(num_inputs=2, num_classes=3)
+    v = FloatTensor([[2, 3]])
+    out = net(v)
+    print(net)
+    print(out)
